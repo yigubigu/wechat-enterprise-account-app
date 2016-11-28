0bb7dbc0510638769b31b910b451ee8d8c712b3d
lory <lory.hou@augmentum.com>
2016-11-28 16:06:11 +0800

    add annotation

--- src/main/java/com/aug/handler/MsgHandler.java
+++ src/main/java/com/aug/handler/MsgHandler.java
@@ -2,12 +2,18 @@ package com.aug.handler;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.WxCpXmlMessage;
 import me.chanjar.weixin.cp.bean.WxCpXmlOutMessage;
 
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MsgHandler extends AbstractHandler  {
 
 	 @Override
