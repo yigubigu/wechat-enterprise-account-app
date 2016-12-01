1755a163ce03ee19239779a71a2fc75bdf321d71
lory <lory.hou@augmentum.com>
2016-12-01 14:02:49 +0800

    add controllers

--- src/main/java/com/aug/controller/DepartController.java
+++ src/main/java/com/aug/controller/DepartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("weichat/depart")
+@RequestMapping("wechat/departs")
 public class DepartController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
